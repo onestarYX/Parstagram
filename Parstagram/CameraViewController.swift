2fe7eda0790f41b04d57898c115f666c11460dba
Yixing Wang <yxonestarw@gmail.com>
2019-04-07 18:51:24 -0700

    finish sign up and sign in

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Yixing Wang on 4/4/19.
+//  Copyright © 2019 wjfwyx@163.com. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
